2f67faa402b4032083ef3a9dddfa5c5581a97efb
Rowan <rowjw97@gmail.com>
2023-11-25 11:50:29 +0000

    Added basic data.

---
+++ mismatched_quotes.js
@@ -0,0 +1,31 @@
+const quotePart1 = [
+    "Ask not what your country can do for you, ",
+    "A bird in the hand is worth ",
+    "Eighty percent of success is ",
+    "Genius is one percent inspiration and ",
+    "He travels the fastest who ",
+    "Hell has no fury like a ",
+    "I think, therefore ",
+    "If you're going through hell, ",
+
+];
+const quotePart2 = [
+    "Ask what you can do for your country.",
+    "Two in the bush.",
+    "showing up.",
+    "ninety-nine percent perspiration.",
+    "travels alone.",
+    "woman scorned.",
+    "I am.",
+    "keep going.",
+
+];
+const quoteAuthor = [
+    "John. F. Kennedy",
+    "Woody Allen",
+    "Thomas Edison",
+    "Rudyard Kipling",
+    "William Congreve",
+    "Rene Descartes",
+    "Winston Churchill."
+];
