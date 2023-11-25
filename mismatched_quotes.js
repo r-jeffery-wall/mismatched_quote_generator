314fdb1664657a56e57a70182077c5e209f04f3f
Rowan <rowjw97@gmail.com>
2023-11-25 12:12:30 +0000

    Expanded list of available quotes.

--- mismatched_quotes.js
+++ mismatched_quotes.js
@@ -7,7 +7,19 @@ const quotePart1 = [
     "Hell has no fury like a ",
     "I think, therefore ",
     "If you're going through hell, ",
-
+    "If at first you don't succeed, ",
+    "If you want something done right, ",
+    "Life is like riding a bicycle. ",
+    "No one can make you feel inferior without ",
+    "Not all those who wander ",
+    "Nothing is certain except for ",
+    "That's one small step for a man, ",
+    "The only thing we have to fear is ",
+    "Three can keep a secret, if ",
+    "T'is better to have loved and lost than ",
+    "What doesn't kill us ",
+    "You must be the change ",
+    "You can fool all of the people some of the time, and "
 ];
 const quotePart2 = [
     "Ask what you can do for your country.",
@@ -18,7 +30,19 @@ const quotePart2 = [
     "woman scorned.",
     "I am.",
     "keep going.",
-
+    "try, try again.",
+    "do it yourself.",
+    "To keep your balance, you must keep moving.",
+    "your consent.",
+    "are lost.",
+    "death and taxes.",
+    "a giant leap for mankind.",
+    "fear itself.",
+    "two of them are dead.",
+    "never to have loved at all.",
+    "makes us stronger",
+    "you wish to see in the world.",
+    "some of the people all of the time, but you can't fool all of the people all of the time."
 ];
 const quoteAuthor = [
     "John. F. Kennedy",
@@ -27,7 +51,20 @@ const quoteAuthor = [
     "Rudyard Kipling",
     "William Congreve",
     "Rene Descartes",
-    "Winston Churchill."
+    "Winston Churchill.",
+    "W. E. Hickson",
+    "Charles-Guillaume Etienne",
+    "Albert Einstein",
+    "Eleanor Roosevelt",
+    "J. R. R. Tolkein",
+    "Benjamin Franklin",
+    "Neil Armstrong",
+    "Franklin D. Roosevelt",
+    "Theodore Roosevelt",
+    "Alfred Lord Tennyson",
+    "Friedrich Nietzsche",
+    "Mahatma Gandhi",
+    "Abraham Lincoln"
 ];
 
 const randNumber = max => Math.floor(Math.random() * max); // Quick helper function for getting a random number within a range.
